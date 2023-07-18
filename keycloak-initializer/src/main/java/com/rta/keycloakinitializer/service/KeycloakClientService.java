f67ad1a8669c1209efcbee1c9668560407f8899d
NachoJ12 <nacho.justel12@gmail.com>
2023-07-18 20:15:31 -0300

    Add setRealmLevelRoles method

--- keycloak-initializer/src/main/java/com/rta/keycloakinitializer/service/KeycloakClientService.java
+++ keycloak-initializer/src/main/java/com/rta/keycloakinitializer/service/KeycloakClientService.java
@@ -1,8 +1,13 @@
 package com.rta.keycloakinitializer.service;
 
 import org.keycloak.admin.client.Keycloak;
+import org.keycloak.admin.client.resource.RealmsResource;
+import org.keycloak.admin.client.resource.RolesResource;
 import org.keycloak.representations.idm.RealmRepresentation;
+import org.keycloak.representations.idm.RoleRepresentation;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 
 @Service
@@ -16,6 +21,9 @@ public class KeycloakClientService {
 
     public void createRealmAndConfigs(String realmName) {
        createRealm(realmName);
+       setRealmLevelRoles(realmName, List.of("APP_USER", "APP_ADMIN"));
+
+
     }
 
     private void createRealm(String realmName){
@@ -25,5 +33,19 @@ public class KeycloakClientService {
         keycloak.realms().create(realm);
         System.out.println("\nRealm " + realmName + " create successful");
     }
+
+    private void setRealmLevelRoles(String realmName, List<String> realmLevelRoles) {
+        RealmsResource realmsResource = keycloak.realms();
+        RolesResource rolesResource = realmsResource.realm(realmName).roles();
+
+        for(String roleName: realmLevelRoles){
+            RoleRepresentation role = new RoleRepresentation();
+            role.setName(roleName);
+            rolesResource.create(role);
+        }
+        System.out.println("\nRealm roles " + realmLevelRoles + " added successfully");
+    }
+
+
 
 }
