51910ebf39fdb1ac60b5165ef666acc831ad7c2e
NachoJ12 <nacho.justel12@gmail.com>
2023-07-18 19:31:34 -0300

    Implement CommandLineRunner for Keycloak initialization
    - Use constructor dependency injection for the KeycloakClientService in the KeycloakInitializerApplication

--- keycloak-initializer/src/main/java/com/rta/keycloakinitializer/KeycloakInitializerApplication.java
+++ keycloak-initializer/src/main/java/com/rta/keycloakinitializer/KeycloakInitializerApplication.java
@@ -1,12 +1,28 @@
 package com.rta.keycloakinitializer;
 
+import com.rta.keycloakinitializer.service.KeycloakClientService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class KeycloakInitializerApplication {
+public class KeycloakInitializerApplication implements CommandLineRunner {
+
+	private KeycloakClientService keycloakClientService;
+
+	public KeycloakInitializerApplication(KeycloakClientService keycloakClientService) {
+		this.keycloakClientService = keycloakClientService;
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(KeycloakInitializerApplication.class, args);
 	}
 
+
+	@Override
+	public void run(String... args) {
+		keycloakClientService.createRealmAndConfigs("ecommerce-rta");
+
+		System.exit(0);
+	}
 }
