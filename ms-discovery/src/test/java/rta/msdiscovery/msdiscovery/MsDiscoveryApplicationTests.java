687826ea9295b07ba8d9f67929ca7a08b4631dc8
NachoJ12 <nacho.justel12@gmail.com>
2023-08-01 18:01:18 -0300

    feat: initialize ms-discovery microservice
    - Add the initial project configuration for the ms-discovery microservice with the Eureka Server and Actuator dependencies.
    - Configuration includes enable Eureka Server with @EnableEurekaServer, set the application name to "ms-discovery" and configure the Eureka Server properties in application.yml.

---
+++ ms-discovery/src/test/java/rta/msdiscovery/msdiscovery/MsDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package rta.msdiscovery.msdiscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
