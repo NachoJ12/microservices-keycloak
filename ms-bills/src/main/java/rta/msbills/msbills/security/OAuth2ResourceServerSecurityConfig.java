4361ec26d02cca40995bd3e56662bd407e8f7a5a
NachoJ12 <nacho.justel12@gmail.com>
2023-08-15 10:39:58 -0300

    feat(ms-bills): enable method-level security
    - Add @EnableGlobalMethodSecurity to enable method-level security checks.
    - This allows the use of annotations like @PreAuthorize and @PostAuthorize.
    - Setting (prePostEnabled = true) ensures security checks before and after method execution.
    - These annotations offer a granular approach to access control for specific functions.

--- ms-bills/src/main/java/rta/msbills/msbills/security/OAuth2ResourceServerSecurityConfig.java
+++ ms-bills/src/main/java/rta/msbills/msbills/security/OAuth2ResourceServerSecurityConfig.java
@@ -2,6 +2,7 @@ package rta.msbills.msbills.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -11,6 +12,7 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class OAuth2ResourceServerSecurityConfig {
 
     @Bean
